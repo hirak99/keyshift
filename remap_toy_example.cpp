22747827d06c274ddbc81e30315e0a1937db53bb
Arnab Bose <hirak99@gmail.com>
2024-09-02 14:24:40 +0530

    Grabbed the keyboard, and restructured the remap code

--- remap_toy_example.cpp
+++ remap_toy_example.cpp
@@ -1,38 +1,54 @@
+// Note that this can be dangerous if it grabs the keyboard and makes Ctrl+C
+// impossible. Always run this with `sudo timeout 20s ./<binary>`.
 #include <fcntl.h>
 #include <linux/input.h>
-#include <unistd.h>
+#include <stdexcept>
 #include <stdio.h>
+#include <unistd.h>
 
-void process_event(struct input_event *ie) {
-    // Example remapping logic
-    if (ie->type == EV_KEY) {
-        if (ie->code == KEY_A && ie->value == 1) { // Key press
-            // Remap KEY_A to KEY_B
-            printf("Remapping A to B\n");
-            // Instead of sending this event, you would send a new KEY_B event
-        } else if (ie->code == KEY_A && ie->value == 0) { // Key release
-            // Handle key release
-        }
-        // Add more remapping logic as needed
+class InputDevice {
+ public:
+  InputDevice(const char* device) {
+    // Open the input device
+    fd = open(device, O_RDONLY);
+    if (fd < 0) {
+      throw std::runtime_error("Error opening device");
     }
-}
+
+    // Grab the device
+    if (ioctl(fd, EVIOCGRAB, 1) < 0) {
+      close(fd);
+      throw std::runtime_error("Error grabbing device");
+    }
+  }
+
+  ~InputDevice() {
+    if (fd >= 0) {
+      // Release the device
+      ioctl(fd, EVIOCGRAB, 0);
+      close(fd);
+    }
+  }
+
+  int get_fd() const { return fd; }
+
+ private:
+  int fd = -1;
+};
 
 int main() {
-    int fd = open("/dev/input/by-id/usb-Drunkdeer_Drunkdeer_G65_US_RYMicro-event-kbd", O_RDONLY);
-    if (fd < 0) {
-        perror("Unable to open input device");
-        return 1;
-    }
+  printf("Wating a sec, release all keys! ...\n");
+  sleep(1);
+  printf("Starting now...\n");
+  InputDevice device(
+      "/dev/input/by-id/usb-Drunkdeer_Drunkdeer_G65_US_RYMicro-event-kbd");
 
-    struct input_event ie;
-    while (read(fd, &ie, sizeof(struct input_event)) > 0) {
-        if (ie.type == EV_KEY) {
-            printf("Key %i %s\n", ie.code, ie.value ? "pressed" : "released");
-            // Process and remap key events here
-        }
+  struct input_event ie;
+  while (read(device.get_fd(), &ie, sizeof(struct input_event)) > 0) {
+    if (ie.type == EV_KEY) {
+      printf("Key %i %s\n", ie.code, ie.value ? "pressed" : "released");
+      // Process and remap key events here
     }
-
-    close(fd);
-    return 0;
+  }
+  return 0;
 }
-
