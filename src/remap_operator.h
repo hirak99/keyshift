47024e22721b9851b3ab6af1df0411bda5d72c79
Arnab Bose <hirak99@gmail.com>
2024-09-03 19:50:04 +0530

    Add emmited_count_ as a convenient timer

--- src/remap_operator.h
+++ src/remap_operator.h
@@ -26,12 +26,25 @@ enum class ActionType {
 
 struct Action {
   ActionType type;
-  // For kKeyEvent, this is the +/- keycode. If negative, it is a release event.
-  // For kActivateMapping, this is the mapping index to switch to.
+  // For kKeyEvent, this is the +/- key_code. If negative, it is a release
+  // event. For kActivateMapping, this is the mapping index to switch to.
   int argument;
 };
 
 using ActionMap = std::unordered_map<int, std::vector<Action>>;
+
+// These will be stored in a stack as new layers get activated.
+struct LayerActivation {
+  int emitted_count;  // When the layer was activated.
+  int key_code;       // key_code that activated this layer. Only positive.
+  const ActionMap prior_mapping;  // Mapping to revert to on deactivation.
+};
+
+// A key that was pressed. All currently held keys will be kept in a vector.
+struct KeyHeld {
+  int emitted_count;  // When the key was held.
+  int key_code;       // key_code that was pressed. Only positive.
+};
 
 class Remapper {
  public:
@@ -40,8 +53,8 @@ class Remapper {
     (void)get_mapping_number("");
   }
 
-  void SetCallback(std::function<void(int, int)> emit_keycode) {
-    emit_keycode_ = emit_keycode;
+  void SetCallback(std::function<void(int, int)> emit_key_code) {
+    emit_key_code_ = emit_key_code;
   }
 
   // Default table should have name "".
@@ -51,8 +64,8 @@ class Remapper {
     mapping[key_code] = actions;
   }
 
-  Action action_key(int keycode) {
-    return Action{ActionType::kKeyEvent, keycode};
+  Action action_key(int key_code) {
+    return Action{ActionType::kKeyEvent, key_code};
   }
 
   Action action_activate_mapping(std::string mapping_name) {
@@ -60,15 +73,15 @@ class Remapper {
                   get_mapping_number(mapping_name)};
   }
 
-  void process(int keycode) {
-    // Check if keycode is in activated mapping stack.
-    if (process_deactivation(keycode)) {
+  void process(int key_code) {
+    // Check if key_code is in activated mapping stack.
+    if (process_deactivation(key_code)) {
       return;
     }
 
-    auto it = current_mapping_.find(keycode);
+    auto it = current_mapping_.find(key_code);
     if (it == current_mapping_.end()) {
-      trigger(keycode);
+      trigger(key_code);
       return;
     }
     for (const Action& action : it->second) {
@@ -79,7 +92,8 @@ class Remapper {
         case ActionType::kActivateMapping:
           auto it = all_mappings_.find(action.argument);
           if (it != all_mappings_.end()) {
-            mappings_pushed_.push({-keycode, current_mapping_});
+            active_layers_.push(
+                LayerActivation{emitted_count_, key_code, current_mapping_});
             current_mapping_ = it->second;
           } else {
             perror(
@@ -103,55 +117,62 @@ class Remapper {
     return index_it->second;
   }
 
-  bool process_deactivation(int keycode) {
-    if (keycode > 0) return false;
-    if (mappings_pushed_.empty()) return false;
-    if (mappings_pushed_.top().first != keycode) return false;
+  bool process_deactivation(int key_code) {
+    if (key_code > 0) return false;
+    // Note: key_code is negative from now on.
+    if (active_layers_.empty()) return false;
+    if (active_layers_.top().key_code != -key_code) return false;
 
-    current_mapping_ = mappings_pushed_.top().second;
-    mappings_pushed_.pop();
-    // TODO: Send release keys for whatever was held after this key.
-    trigger(keycode);
+    current_mapping_ = active_layers_.top().prior_mapping;
+    active_layers_.pop();
+    // TODO: Release all the currently pressed keys after this was activated.
+    trigger(key_code);
     return true;
   }
 
-  void trigger(int keycode) {
-    const int press = (keycode > 0 ? 1 : 0);
-    if (keycode < 0) keycode = -keycode;
+  void trigger(int key_code) {
+    const int press = (key_code > 0 ? 1 : 0);
+    if (key_code < 0) key_code = -key_code;
     if (press == 1) {
-      keys_held_.push_back(keycode);
+      keys_held_.push_back(KeyHeld{emitted_count_, key_code});
     } else {
-      if (std::find(keys_held_.begin(), keys_held_.end(), keycode) ==
-          keys_held_.end()) {
+      auto it = std::find_if(
+          keys_held_.begin(), keys_held_.end(),
+          [key_code](const KeyHeld& kh) { return kh.key_code == key_code; });
+      if (it == keys_held_.end()) {
         // This key is not held.
+        perror("WARNING: Ignoring key release event as key not held.");
         return;
       }
-      keys_held_.erase(
-          std::remove_if(keys_held_.begin(), keys_held_.end(),
-                         [keycode](int x) { return x == keycode; }));
+      keys_held_.erase(std::remove_if(
+          keys_held_.begin(), keys_held_.end(),
+          [key_code](KeyHeld& kh) { return kh.key_code == key_code; }));
     }
-    if (emit_keycode_ != nullptr) {
-      emit_keycode_(keycode, press);
+    if (emit_key_code_ != nullptr) {
+      emit_key_code_(key_code, press);
     }
+    ++emitted_count_;
   }
 
   // Do not use this directly, use get_mapping_number().
   std::unordered_map<std::string, int> mapping_name_to_index_;
 
   // Outer key is a lookup from the mapping name.
-  // Inner key is the condition, a keycode. Negative indicates on release.
+  // Inner key is the condition, a key_code. Negative indicates on release.
   std::unordered_map<int, ActionMap> all_mappings_;
 
   // Currently activated mapping.
   ActionMap& current_mapping_;
   // Previous mappings. This is used as mappings get deactivated.
-  // Pair of keycode, mapping_index.
-  std::stack<std::pair<int, const ActionMap>> mappings_pushed_;
+  // Pair of key_code, mapping_index.
+  std::stack<LayerActivation> active_layers_;
 
-  // Current keys being held. This is based on what's emitted.
-  // Contains only positive values.
-  std::vector<int> keys_held_;
+  // Current keys being pressed. This is based on what's emitted.
+  std::vector<KeyHeld> keys_held_;
 
-  // On process(), keycodes are emitted via this callback.
-  std::function<void(int, int)> emit_keycode_ = nullptr;
+  // How many keys emitted so far. Acts as a convenient time tag for events.
+  int emitted_count_ = 0;
+
+  // On process(), key_codes are emitted via this callback.
+  std::function<void(int, int)> emit_key_code_ = nullptr;
 };
